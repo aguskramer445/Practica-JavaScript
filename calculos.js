f6bba5ef11979e25dc73f1b1ccfc443f20f8e4ba
aguskramer445 <136341916+aguskramer445@users.noreply.github.com>
2023-08-07 10:47:02 -0300

    Add files via upload

---
+++ calculos.js
@@ -0,0 +1,32 @@
+//Punto 2
+console.log('La suma es igual a', 50+50)
+console.log('La resta es igual a', 50-50)
+console.log('La multipicacion es igual a', 50*30)
+console.log('La division es igual a', 20/20)
+//Punto 3
+let  suma = 50+50
+let resta = 50-50
+let multipicacion = 50*30
+let division = 20/20
+console.log("Variable suma", suma, "Variable resta", resta,  "Variable multipicacion", multipicacion, "Variable division", division )
+alert("El resultado de la suma es 100")
+alert("El resultado de la resta es 0")
+alert("El resultado de la multiplicacion es 1500")
+alert("El resultado de la division es 1")
+//Punto 4
+alert("Mi edad es de "+(2023-2008))
+//Punto 5
+prompt("Ingresa tu año de nacimiento")
+//Punto 6
+let nombreAlumno = prompt ("Ingrese su nombre")
+alert("Bienvenido " + nombreAlumno)
+//Punto 7
+let nombre = prompt ("Ingrese su nombre")
+let apellido= prompt ("Ingrese su apellido")
+alert("Bienvenido " + nombre + " " + apellido)
+console.log(nombre + apellido)
+//Punto 8
+let diez = 10
+let tres = 3
+let resultadoMultiplicacion = 10 * 3
+alert("El resultado es " + resultadoMultiplicacion)
